794e19622753eeba0df9d05a99daf66e1e86151e
miishchuk <miishchuk@sberinsur.ru>
2024-05-06 18:25:45 +0300

    add log file

--- src/main/java/ru/sberinsur/data/entity/SchemaTable.java
+++ src/main/java/ru/sberinsur/data/entity/SchemaTable.java
@@ -3,14 +3,12 @@ package ru.sberinsur.data.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "validation_schemas")
 public class SchemaTable {
 
     @Id
